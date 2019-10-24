f8411dff6b9db22dcaa827b22f8b2ca905ac0056
yabaze.t <yabaze.t@roi.actcorp.in>
2019-10-24 14:42:05 +0530

    Dynamic API Implemented Successfully

--- src/controllers/firebase-access-controller.controller.ts
+++ src/controllers/firebase-access-controller.controller.ts
@@ -1,37 +1,59 @@
-// Uncomment these imports to begin using these cool features!
-
-// import {inject} from '@loopback/context';
-import { get } from '@loopback/rest'
+import {
+  Request,
+  RestBindings,
+  get,
+  ResponseObject,
+  param,
+} from '@loopback/rest';
+import {inject} from '@loopback/context';
 
 export class FirebaseAccessControllerController {
   firebase: any;
+
   db: any;
 
-  constructor() {
-    this.firebase = require("firebase");
-    this.firebase.initializeApp({
-      serviceAccount: "./firebaseService.json",
-      databaseURL: "https://yabaze-profile.firebaseio.com/"
-    });
+  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {
+    this.firebase = require('firebase');
+    if (!this.firebase.apps.length) {
+      this.firebase.initializeApp({
+        serviceAccount: './firebaseService.json',
+        databaseURL: 'https://yabaze-profile.firebaseio.com/',
+      });
+    }
     this.db = this.firebase.database();
-
   }
 
-  @get('/rest/v1/skills')
-  getSkills(): any {
-
-    var ref = this.db.ref("/yabaze/skills");
+  @get('/rest/v1/{userName}/{pageName}')
+  async getSkills(
+    @param.path.string('userName') userName: string,
+    @param.path.string('pageName') pageName: string,
+  ) {
+    var ref = this.db.ref('/' + userName + '/' + pageName);
 
     var skills: any;
+    var successMessage: String;
 
-    ref.on("value", function (snapshot: any) {
-      skills = snapshot.val();
-    }, function (errorObject: any) {
-      console.log("The read failed: " + errorObject.code);
-    });
+    ref.on(
+      'value',
+      function(snapshot: any) {
+        skills = snapshot.val();
+      },
+      function(errorObject: any) {
+        console.log('The read failed: ' + errorObject.code);
+      },
+    );
 
-    return skills;
+    if (skills == undefined) {
+      successMessage = 'Data Not Found';
+    } else {
+      successMessage = 'Success';
+    }
+
+    return {
+      data: skills,
+      message: successMessage,
+      date: new Date(),
+      url: this.req.url,
+    };
   }
 }
-
-
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,1 @@
-export * from './ping.controller';
-export * from './hello.controller';
-export * from './sample.controller';
-export * from './about-me.controller';
 export * from './firebase-access-controller.controller';
